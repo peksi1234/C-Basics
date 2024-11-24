3e4ac8147b7ba0e9c5833636022caa07184ce8a3
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-24 22:27:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        string numberAsText = "1234";
+
+ 
+        int number = Convert.ToInt32(numberAsText);
+
+        Console.WriteLine(number);
+
+        Console.ReadKey();
+    }
+}
