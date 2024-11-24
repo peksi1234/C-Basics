187b9052fbd43fb5ce4d130fc4f1f855df41a0e7
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-24 22:30:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_and_output_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name:");
+            string Username = Console.ReadLine();
+            Console.WriteLine("Hello, " + Username + "! Hello and welcome");
+
+            Console.ReadKey();
+
+        }
+    }
+}
