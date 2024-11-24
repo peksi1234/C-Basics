1deb90e9e9cf866dfdf80f0520f01d565e64edd8
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-24 22:34:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1_variables_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sebastian";
+            int num1 = 1;
+        int num2 = 2;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(name);
+
+            Console.ReadKey();
+        }
+    }
+}
