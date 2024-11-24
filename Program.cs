20967adf5f647ab5d660636e1d355fa3b41c0417
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-24 23:35:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 5)
+            {
+                Console.WriteLine("counter is " + counter);
+                counter++;
+            }
+            
+            Console.WriteLine("Counter is 6");
+            Console.WriteLine("Loop has finished");
+            Console.ReadLine();
+        }
+    }
+}
