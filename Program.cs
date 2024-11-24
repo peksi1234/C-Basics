bf193e0ab2418bfb9e63f6dd94ff451e8876ee8a
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-24 23:32:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Your eligble to vote.");
+            }
+            else
+            {
+                Console.WriteLine("Your not eligble to vote.");
+
+            }
+        }
+    }
