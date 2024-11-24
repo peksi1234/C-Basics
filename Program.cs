3b7074361e7468b253d4b43f522c781d9bb9d2ff
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-24 23:34:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace _1_switch_statements_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade");
+            int grade = Convert.ToInt32(Console.ReadLine());
+        }
+
+        switch ()
+    }
+}
