ffb84cb20e3ae1e76d821dd8b2a34df76fda1952
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-27 16:10:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OddEvenChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter a number:");
+
+            
+            int number;
+            bool isValidInput = int.TryParse(Console.ReadLine(), out number);
+
+            if (isValidInput)
+            {
+                
+                if (number % 2 == 0)
+                {
+
+                    Console.WriteLine($"The number {number} is even.");
+                }
+                else
+                {
+                  
+                    Console.WriteLine($"The number {number} is odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
