6f4ed698269791f3aa50a7dd06b1e3ce9dbed11b
peksi1234 <sebastian.osterberg06@gmail.com>
2024-11-27 16:08:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_operators_in_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            int num4 = 4;
+            int num5 = 5;
+            int num6 = 6;
+
+            Console.WriteLine(num1 + num2);
+
+            int product = num3 * num4;
+            Console.WriteLine(product);
+
+            int remainder = num5 %= num6;
+            Console.WriteLine(remainder);
+
+            Console.ReadKey();
+        }
+    }
+}
