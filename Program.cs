c6e8c2c7cbc56317ce39274435b286ff965ae22d
peksi1234 <sebastian.osterberg06@gmail.com>
2024-12-10 16:52:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AgeVerificationAndControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Counter = 1;
+
+            Console.WriteLine("Loop output:");
+            while (Counter <= 5)
+            {
+
+                Console.WriteLine($"Counter value: {Counter}");
+
+
+                Counter++;
+            }
+
+            Console.WriteLine("\nSwitch statement output:");
+            switch (Counter)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                default:
+                    Console.WriteLine("Invalid number");
+                    break;
+            }
+
+            var name = "John";
+
+            Console.WriteLine($"\nWelcome, {name}! The loop ran and counted up to 5. The switch statement result was based on the final Counter value.");
+
+            Console.ReadLine();
+        }
+    }
+}
