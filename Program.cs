3744682bf28e3e25a52d50f90d40e8789ce3baba
peksi1234 <sebastian.osterberg06@gmail.com>
2024-12-11 17:04:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class AreaCalculationTask
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the width of the triangle: ");
+        double width = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the height of the triangle: ");
+        double height = double.Parse(Console.ReadLine());
+
+        double area = CalculateTriangleArea(width, height);
+        Console.WriteLine($"The area of the triangle is: {area}");
+    }
+
+    static double CalculateTriangleArea(double width, double height)
+    {
+        return (width * height) / 2;
+    }
+}
