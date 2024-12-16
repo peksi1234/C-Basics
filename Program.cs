eb0d42043e9f749edc93c892ae1ce85161d6e309
peksi1234 <sebastian.osterberg06@gmail.com>
2024-12-16 18:14:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LogicalError1
+{
+    internal class LogicalError1
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 5, 30, 25 };
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"The maximum value in the array is: {max}");
+            Console.ReadLine();
+        }
+    }
+}
