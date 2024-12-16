3989fad91d8e73d4a00816aa8cb87d09d40b05a0
peksi1234 <sebastian.osterberg06@gmail.com>
2024-12-16 21:49:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicError2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 34;
+
+            if (age > 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+            else if (age > 30)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age > 18)
+            {
+                Console.WriteLine("Over 18");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
